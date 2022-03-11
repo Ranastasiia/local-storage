076b57d993aaab3b4d7aace14fcae12677196f4e
Ranastasiia <nastuxa1971@gmail.com>
2022-03-11 13:48:44 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ let playerName = document.getElementById('playerName')
 let name = document.querySelector('#name span')
 let start = document.getElementById('start')
 let click = document.getElementById('click')
+let clear = document.getElementById('clear')
+let game = document.querySelector('.game')
+let modal = document.querySelector('.modal')
+let endGameModal = document.querySelector('.endGameModal')
+
+console.log(localStorage.getItem('arr'))
+let date;
+let diff
+
 let counter = 0
 let clickNum = document.querySelector('#clickNum span')
 
@@ -14,13 +23,17 @@ playerName.addEventListener('input', Check)
 start.addEventListener('click', Start)
 click.addEventListener('click', Click)
 
+clear.addEventListener('click', function () {
+    localStorage.clear()
+})
+
 function Check (){
     start.disabled = playerName.value === ''
 }
 function Start(){
     name.textContent = playerName.value
     playerName.value = ''
-    let date = new Date(0, 0, 0, 0, 0, 0, 0)
+    date = new Date(0, 0, 0, 0, 0, 0, 0)
     getTimer = setInterval(function (){
         date.setMilliseconds(date.getMilliseconds()+10)
         sec.textContent = zero(date.getSeconds())
@@ -33,9 +46,20 @@ function Click(){
     counter++
     clickNum.textContent = counter
     if (counter===10){
+        let endGame = new Date(0, 0, 0, 0, 0, 0, 0)
+        diff = date-endGame
+        counter = 0
         click.disabled = true
         start.disabled = true
         clearInterval(getTimer)
+        // game.style.display = 'none'
+        modal.style.display = 'flex'
+        modal.innerHTML = `Name: ${name.textContent}<br>Time: ${sec.textContent}.${ms.textContent}`
+        getJson()
+        setTimeout(function (){
+            modal.style.display = 'none'
+            endGameModal.style.display = 'block'
+        }, 3000)
     }
 }
 
@@ -44,4 +68,19 @@ function zero(n){
 }
 function zeroms(n){
     return (n<100) ? ('0'+n) : n
+}
+
+function createTable(){
+
+}
+
+function getJson(){
+    let array = JSON.parse(localStorage.getItem('arr'))
+    array.push(new Object({'name':name.textContent, 'diff':diff, 'time': `${sec.textContent}.${ms.textContent}`}))
+
+
+
+    let json = JSON.stringify(array)
+    localStorage.setItem('arr', json)
+    console.log(localStorage.getItem('arr'))
 }
